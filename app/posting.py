18389213d1fd260e8d4f30a11a6360d01121d77e
TechWTy <you@example.com>
2023-03-26 00:27:05 -0600

    Trying To get Keys from response

--- app/posting.py
+++ app/posting.py
@@ -11,8 +11,10 @@ ACCESS_TOKEN = os.environ.get('ZB_PROMO')
 # print(ACCESS_TOKEN, 'ACCESS_TOKEN')
 
 if not ACCESS_TOKEN:
-    TOKEN_OBJ = keys.get_secret("ZB_PROMO")
+    TOKEN_OBJ = json.loads(keys.get_secret("ZB_PROMO"))
+  
     ACCESS_TOKEN = TOKEN_OBJ.get('ZB_PROMO')
+    print("ACCESS_TOKEN", ACCESS_TOKEN)
 # Load environment variables from .env file
 
 
