a6676447f892fe7b277dfb6d6c0b2a8dc9f3a82d
TechWIthTy <37724661+TechWithTy@users.noreply.github.com>
2023-03-25 20:59:59 -0600

    Create __AWS__.py

---
+++ __AWS__.py
@@ -0,0 +1,38 @@
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
+
+import boto3
+from botocore.exceptions import ClientError
+
+push_bullet = "PUSH_BULLET"
+zb_promo = "ZB_PROMO"
+twin_nem = "TWIN_NEM"
+def get_secret(secret_str: str) -> str:
+
+    
+    region_name = "us-east-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_str
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+    print(secret)
+    return(secret)
+    # Your code goes here.
+get_secret(push_bullet)
