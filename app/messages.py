b30aa24823880acf2a91c34847073db68d21c571
TechWTy <you@example.com>
2023-03-26 23:20:32 -0600

    used schedule instad of while loop

--- app/messages.py
+++ app/messages.py
@@ -7,12 +7,7 @@ Gushers
 """
 
 test_text = f"""\
-69 
-Gets Jumped
-At Gym
-https://www.youtube.com/watch?v=MlWcuDOUTTA
-testing a new Gort bot i turned notifications off use the side chat in the mean time
-©ALPHA
+
 
 """
 mlightm = f"""\
