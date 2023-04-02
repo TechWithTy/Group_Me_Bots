9e8f871951d61b288154c782f5eb20008299a7c4
TechWTy <you@example.com>
2023-04-02 11:20:28 -0600

    edited message with instructions

--- app/posting.py
+++ app/posting.py
@@ -79,14 +79,14 @@ def send_message_to_groups(new_bots: list, message: str) -> str:
             try:
                 # response = requests.post(
                 # url, headers=headers, data=json.dumps(payload))
-                print(new_bots)
+                
                 print(f"Bot: '{bot_id}' sent message to group '{group_id}'. Status code: ")
                            
             except Exception:
                 pass
                 raise Exception(
                     f"Error sending message by bot '{bot_id}' to group '{group_id}'. Status code: ")
-                            
+    print(new_bots)                        
     return "Message sent to all groups successfully."
 
 
