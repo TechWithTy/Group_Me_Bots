e72e3c0af96697493e841b948865d4a72c671496
TechWTy <you@example.com>
2023-05-10 21:52:14 -0600

    timed post push

--- app/messages.py
+++ app/messages.py
@@ -156,12 +156,12 @@ And the best part? If you sign up for any of these apps through our referral lin
 message_duration_data = [
     {'times': ['12:45', '20:00'],  'message':looking_to_make_money, 'images': [
         'https://res.cloudinary.com/djao481zq/image/upload/v1683772469/Misc/GM/Affiliate%20Links/smklxqxnwntskndpm0dv.jpg',]},
-   {'times':['12:00','17:00'],'duration':  26, 'message':prime_ebt, 'images': [
+   {'times':['12:00','17:00'], 'message':prime_ebt, 'images': [
        'https://res.cloudinary.com/djao481zq/image/upload/v1683187215/Misc/GM/Amazon%20AFL/mqsea1cnyda3ap8zng3g.jpg',]},
    
-    {'times': ['10:30', '14:00'], 'message': amazon_drop, 'images': [
+    {'times': ['10:30', '14:00'],'duration': 28, 'message': amazon_drop, 'images': [
         'https://res.cloudinary.com/djao481zq/image/upload/v1683185967/Misc/GM/Amazon%20AFL/mmozad0hpo29hnn4o5o7.jpg',]},
-    {'times': ['13:30', '18:30'], 'message': prime_student, 'images': [
+    {'times': ['13:30', '18:30'],'duration': 32, 'message': prime_student, 'images': [
      'https://res.cloudinary.com/djao481zq/image/upload/v1683186495/Misc/GM/Amazon%20AFL/mxbfgql2cnpxrkqwlagb.jpg',]},
 
     {'duration': 30, 'message': spray_bottle, 'images': [
